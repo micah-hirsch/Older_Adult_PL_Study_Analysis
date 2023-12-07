6f842cb2a3984926c437f0ac699abfea746fa417
micah-hirsch <micahhirsch06@gmail.com>
2023-12-07 15:12:00 -0500

    Started Manuscript Analysis Folder

---
+++ Manuscript Analyses/data_processing_updated.R
@@ -0,0 +1,16 @@
+# Data Preprocessing
+
+# Author: Micah E. Hirsch (mhirsch@fsu.edu)
+# Date: 12/7/2023
+
+# R Version: 4.3.2
+# Purpose: To prepare data for analysis
+
+# Packages
+
+library(rio) #install.packages("rio")
+library(tidyverse) #install.packages("tidyverse")
+library(remotes) #install.packages("remotes")
+library(autoscore) #remotes::install_github("autoscore/autoscore")
+library(SnowballC) #install.packages("SnowballC")
+
